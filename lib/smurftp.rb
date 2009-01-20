9e69bdc2fe527f4178730b108d12c475c79fafd1
agruner <agruner@sesame-ed79be29.(none)>
2009-01-20 15:01:00 -0800

    [smurftp.rb] added to_regex monkey patch to String

--- lib/smurftp.rb
+++ lib/smurftp.rb
@@ -6,6 +6,13 @@ require 'readline'
 # require 'rubygems'
 # other dependencies
 
+# a bit of monkey patching
+class String
+  def to_regex
+    return /#{self}/
+  end
+end
+
 %w(version configuration shell).each do |file|
   require File.join(File.dirname(__FILE__), 'smurftp', file) 
 end
